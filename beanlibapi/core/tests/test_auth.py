128ec0cbdab16c0a5143ac7ab07f3244bb60e5d6
ask4git <ask4git@gmail.com>
2024-09-29 18:56:10 +0900

    edit `TestBean` to class base test

--- beanlibapi/core/tests/test_auth.py
+++ beanlibapi/core/tests/test_auth.py
@@ -19,8 +19,27 @@ def test_registration_should_create_user():
             'password2': 'mytopsecret1'
         }
     )
-    assert response.status_code == 204
+    assert response.status_code == 201
     user_model = get_user_model()
     user = user_model.objects.get(username='testuser1')
     assert user.email == 'test@test2.com'
     assert user.username == 'testuser1'
+
+
+@pytest.mark.django_db
+def test_registration_should_create_user2():
+    client = APIClient()
+    response = client.post(
+        '/core/auth/sign-up/',
+        {
+            'username': 'testuser2',
+            'email': 'test@test2.com',
+            'password1': 'mytopsecret1',
+            'password2': 'mytopsecret1'
+        }
+    )
+    assert response.status_code == 201
+    user_model = get_user_model()
+    user = user_model.objects.get(username='testuser2')
+    assert user.email == 'test@test2.com'
+    assert user.username == 'testuser2'
