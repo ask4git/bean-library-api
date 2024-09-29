f11aba355e65baf71ee3e54a2b57f093004a821b
ask4git <ask4git@gmail.com>
2024-09-30 03:26:39 +0900

    edit `test_beans.py`

--- beanlibapi/core/tests/test_beans.py
+++ beanlibapi/core/tests/test_beans.py
@@ -6,63 +6,54 @@ from rest_framework.test import APIClient
 
 @pytest.mark.django_db
 class TestBean:
-    @pytest.fixture(autouse=True)
-    def setup(self):
-        self.client = APIClient()
+    # @pytest.fixture(autouse=True)
+    @classmethod
+    def setup_class(cls):
+        cls.client = APIClient()
+        cls.data = {
+            "name": "Colombia Granja La Esperanza Cerro Azul Geisha Honey",
+            "region": "colombia",
+            "variety": "geisha",
+            "process": "natural"
+        }
 
-    def test_bean_list(self, bean1, bean2, bean3_inactive):
-        cnt = m.Bean.objects.all().count()
-        assert cnt == 3
+    def setup_method(self):
+        self.client.force_authenticate(user=None)
 
-    def test_bean_list2(self):
-        flag = False
-        assert flag is False
+    def test_query_should_not_return_inactive_data(self, bean1, bean2, bean1_inactive):
+        assert m.Bean.objects.all().count() == 2
 
-    def test_bean_list3(self):
-        flag = True
-        assert flag is True
-
-    def test_bean_get(self, bean1, bean2, bean3_inactive):
+    def test_should_not_return_inactive_bean_via_get(self, bean1, bean2, bean1_inactive):
         response = self.client.get('/core/bean/')
         assert response.status_code == status.HTTP_200_OK
         assert len(response.data) == 2
 
-    def test_superuser_can_create_bean_via_post(self, superuser, bean1, bean2, bean3_inactive):
+    def test_authenticated_user_can_create_bean_via_post(self, superuser, bean1, bean2, bean1_inactive):
         self.client.force_authenticate(user=superuser)
+        self.num_of_beans = 2
 
         response = self.client.get('/core/bean/')
         assert response.status_code == status.HTTP_200_OK
-        assert len(response.data) == 2
+        assert len(response.data) == self.num_of_beans
 
-        data = {
-            "name": "Colombia Granja La Esperanza Cerro Azul Geisha Honey",
-            "region": "colombia",
-            "variety": "geisha",
-            "process": "natural"
-        }
-        response = self.client.post('/core/bean/', data=data, format='json')
-        print(response.data)
+        response = self.client.post('/core/bean/', data=self.data, format='json')
         assert response.status_code == status.HTTP_201_CREATED
+
         response = self.client.get('/core/bean/')
         assert response.status_code == status.HTTP_200_OK
-        assert len(response.data) == 3
+        assert len(response.data) == self.num_of_beans + 1
 
-    def test_user_cannot_create_bean_via_post(self, user2, bean1, bean2, bean3_inactive):
+    def test_user_cannot_create_bean_via_post(self, user2, bean1, bean2, bean1_inactive):
         self.client.force_authenticate(user=user2)
+        self.num_of_beans = 2
 
         response = self.client.get('/core/bean/')
         assert response.status_code == status.HTTP_200_OK
-        assert len(response.data) == 2
+        assert len(response.data) == self.num_of_beans
 
-        data = {
-            "name": "Colombia Granja La Esperanza Cerro Azul Geisha Honey",
-            "region": "colombia",
-            "variety": "geisha",
-            "process": "natural"
-        }
-        response = self.client.post('/core/bean/', data=data, format='json')
-        print(response.data)
+        response = self.client.post('/core/bean/', data=self.data, format='json')
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
         response = self.client.get('/core/bean/')
         assert response.status_code == status.HTTP_200_OK
-        assert len(response.data) == 2
+        assert len(response.data) == self.num_of_beans
