a08daea4ecb2715e6813b589a6c7f076fdeb7a39
ask4git <ask4git@gmail.com>
2024-10-15 21:52:09 +0900

    feat: add `settings.development`

---
+++ beanlibapi/settings/development.py
@@ -0,0 +1,27 @@
+import environ
+from .base import *
+
+env = environ.Env()
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+env_file = os.path.join(BASE_DIR, '.env.development')
+if os.path.exists(env_file):
+    environ.Env.read_env(env_file)
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env('SECRET_KEY')
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
+    }
+}
