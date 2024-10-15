a58b549eba7b3b7a7a5b6dec090eb5c218b2d9e5
ask4git <ask4git@gmail.com>
2024-10-16 02:03:56 +0900

    feat: edit GitHub action `django.yml`

--- beanlibapi/settings/test.py
+++ beanlibapi/settings/test.py
@@ -1,10 +1,17 @@
+import environ
 from .base import *
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+env = environ.Env()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
+env_file = os.path.join(BASE_DIR, '.env.local')
+if os.path.exists(env_file):
+    environ.Env.read_env(env_file)
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env('SECRET_KEY')
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
