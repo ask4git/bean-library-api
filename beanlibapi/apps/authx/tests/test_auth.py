a3cbe007695395a450bfe6962a0d5e24138b0cdc
ask4git <ask4git@gmail.com>
2024-10-27 05:09:44 +0900

    :test_tube: Remove test `test_registration_should_create_user`

--- beanlibapi/apps/authx/tests/test_auth.py
+++ beanlibapi/apps/authx/tests/test_auth.py
@@ -10,22 +10,6 @@ class TestUsers:
     def setup_class(cls):
         cls.client = APIClient()
 
-    def test_registration_should_create_user(self):
-        response = self.client.post(
-            '/dj-rest-auth/registration/',
-            {
-                'username': 'testuser1',
-                'email': 'testuser1@test1.com',
-                'password1': 'mytopsecret1',
-                'password2': 'mytopsecret1'
-            }
-        )
-        assert response.status_code == 201
-        user_model = get_user_model()
-        user = user_model.objects.get(username='testuser1')
-        assert user.email == 'testuser1@test1.com'
-        assert user.username == 'testuser1'
-
     def test_registration_should_create_user2(self):
         response = self.client.post(
             '/auth/sign-up/',
