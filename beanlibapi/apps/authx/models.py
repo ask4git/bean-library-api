0129f89ec70be7f21be137adee2c3d595703810c
ask4git <ask4git@gmail.com>
2024-10-31 04:45:35 +0900

    :art: Add field `email` in User model

--- beanlibapi/apps/authx/models.py
+++ beanlibapi/apps/authx/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-
 from beanlibapi.apps.authx import model_managers as mm
 
 
 class User(AbstractUser):
+    email = models.EmailField(null=False, blank=False)
     deleted = models.BooleanField(db_index=True, null=True, blank=True)
     deleted_at = models.DateTimeField(db_index=True, null=True, blank=True)
     phone_number = models.CharField(max_length=15, blank=True, null=True)
