2e8270914c89e9574e1aa19094fae37a6aa1bd4b
ask4git <ask4git@gmail.com>
2024-11-28 12:47:15 +0900

    :hammer: Add serializer `CafeSerializer`

--- beanlibapi/apps/core/serializers.py
+++ beanlibapi/apps/core/serializers.py
@@ -7,7 +7,7 @@ from dj_rest_auth.registration.serializers import (
 from dj_rest_auth.serializers import (
     LoginSerializer as _LoginSerializer,
 )
-from beanlibapi.apps.core.models import Bean
+from beanlibapi.apps.core.models import Bean, Attachment, Cafe
 
 user_model = get_user_model()
 
@@ -15,13 +15,13 @@ user_model = get_user_model()
 class CustomRegisterSerializer(_RegisterSerializer):
     first_name = s.CharField(required=True)
     last_name = s.CharField(required=True)
-    phone_number = s.CharField(required=True)  # 커스텀 필드 추가
+    phone_number = s.CharField(required=True)
 
     def get_cleaned_data(self):
         data = super().get_cleaned_data()
         data['first_name'] = self.validated_data.get('first_name', '')
         data['last_name'] = self.validated_data.get('last_name', '')
-        data['phone_number'] = self.validated_data.get('phone_number', '')  # 커스텀 필드
+        data['phone_number'] = self.validated_data.get('phone_number', '')
         return data
 
 
@@ -42,3 +42,16 @@ class UserSerializer(s.ModelSerializer):
         return user
 
 
+class AttachmentSerializer(s.ModelSerializer):
+    class Meta:
+        model = Attachment
+        fields = ['name', 'format']
+
+
+class CafeSerializer(s.ModelSerializer):
+    class Meta:
+        model = Cafe
+        fields = '__all__'
+        extra_kwargs = {
+            'owner': {'required': False}
+        }
